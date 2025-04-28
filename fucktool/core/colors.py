cab7e5ecf288f6bad6db08c9b7c57d2cd6e24636
Zalgo-Dev <29240471+Zalgo-Dev@users.noreply.github.com>
2025-04-28 20:46:54 +0200

    refonte plus propre et ajout de promptkit

---
+++ fucktool/core/colors.py
@@ -0,0 +1,77 @@
+# 🌈 Palette de couleurs
+class COLOR:
+    RESET = "\033[0m"
+    BOLD = "\033[1m"
+    DIM = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    REVERSE = "\033[7m"
+    HIDDEN = "\033[8m"
+
+    # ⚪ White and gray
+    BRIGHT_WHITE = "\033[97m"
+    WHITE = "\033[37m"
+    LIGHT_GRAY = "\033[37m"
+    SILVER = "\033[38;5;247m"
+    GRAY = "\033[90m"
+    DARK_GRAY = "\033[38;5;235m"
+    BRIGHT_BLACK = "\033[90m"
+    BLACK = "\033[30m"
+
+    # 🔴 Red
+    LIGHT_RED = "\033[38;5;203m"
+    BRIGHT_RED = "\033[91m"
+    NEON_RED = "\033[38;5;196m"
+    RED = "\033[31m"
+    DARK_RED = "\033[38;5;124m"
+
+    # 🟢 Green
+    LIGHT_GREEN = "\033[92m"
+    BRIGHT_GREEN = "\033[92m"
+    NEON_GREEN = "\033[38;5;46m"
+    MINT_GREEN = "\033[38;5;48m"
+    GREEN = "\033[32m"
+    DARK_GREEN = "\033[38;5;28m"
+
+    # 🟡 Yellow
+    LIGHT_YELLOW = "\033[38;5;228m"
+    BRIGHT_YELLOW = "\033[93m"
+    NEON_YELLOW = "\033[38;5;226m"
+    YELLOW = "\033[33m"
+    GOLD = "\033[38;5;214m"
+    DARK_YELLOW = "\033[38;5;136m"
+
+    # 🟠 Orange
+    LIGHT_ORANGE = "\033[38;5;215m"
+    BRIGHT_ORANGE = "\033[38;5;214m"
+    NEON_ORANGE = "\033[38;5;202m"
+    ORANGE = "\033[38;5;208m"
+    SUNSET_ORANGE = "\033[38;5;166m"
+    DARK_ORANGE = "\033[38;5;130m"
+
+    # 🔵 Blue
+    SKY_BLUE = "\033[38;5;117m"
+    LIGHT_BLUE = "\033[94m"
+    BRIGHT_BLUE = "\033[94m"
+    NEON_BLUE = "\033[38;5;51m"
+    TURQUOISE = "\033[38;5;45m"
+    BLUE = "\033[34m"
+    DARK_BLUE = "\033[38;5;19m"
+
+    # 🟣 Purple
+    PINK = "\033[38;5;205m"
+    MAGENTA = "\033[38;5;200m"
+    LIGHT_PURPLE = "\033[95m"
+    BRIGHT_PURPLE = "\033[95m"
+    NEON_PURPLE = "\033[38;5;201m"
+    PURPLE = "\033[35m"
+    DARK_PURPLE = "\033[38;5;54m"
+
+    # 🟦 Cyan
+    AQUA = "\033[38;5;14m"
+    LIGHT_CYAN = "\033[38;5;51m"
+    BRIGHT_CYAN = "\033[96m"
+    NEON_CYAN = "\033[38;5;50m"
+    CYAN = "\033[36m"
+    DARK_CYAN = "\033[38;5;30m"
