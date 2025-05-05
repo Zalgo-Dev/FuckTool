c72724035e70cd9c4bcab689c57d42ff254abad9
Zalgo-Dev <29240471+Zalgo-Dev@users.noreply.github.com>
2025-05-05 15:10:25 +0200

    + add new core command manager and debug command manager

--- fucktool/main.py
+++ fucktool/main.py
@@ -2,11 +2,14 @@ import os
 import sys
 import time
 from pathlib import Path
+import importlib
+import pkgutil
+import commands
 
 from core.colors import NEON_RED, WHITE, RESET
 from core.header import display_header
 from core.debug import handle_exception, debug_print
-from commands.handler import get_all_commands, handle_command
+from core.command_manager import get_all_commands, handle_command
 
 # Nouvelle importation du système d'input amélioré
 try:
@@ -15,6 +18,8 @@ try:
 except ImportError:
     USE_PROMPT_TOOLKIT = False
 
+for loader, name, is_pkg in pkgutil.iter_modules(commands.__path__):
+    importlib.import_module(f"commands.{name}")
 
 def main():
     """Boucle principale du terminal"""
@@ -22,7 +27,8 @@ def main():
     COMMANDS = get_all_commands()
 
     display_header()
-    debug_print(f"Debug mode enabled.\n")
+    debug_print(f"Debug mode enabled.")
+    debug_print(f"Loaded commands: {get_all_commands()}\n")
 
     try:
         while True:
